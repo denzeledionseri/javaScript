a33f6febc8011faa4f37de487aad5989c14b5692
denzeledionseri <denzeledionseri@gmail.com>
2023-01-02 19:01:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//Write code that prints name, height and country(state)
+function myInfo(name, height, country) {
+    console.log("Denzel" + "Edionseri");
+
+    console.log("6'2");
+
+    console.log("USA");
+}
+
+myInfo("Denzel" + "Edionseri");
+alert("Denzel" + "Edionseri");
+
+myInfo("6'2");
+alert("6'2");
+
+myInfo("USA");
+alert("USA");
